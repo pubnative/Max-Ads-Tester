25d53fbd4420614e048233a91be14b58f35d15e5
Abanoub Barayo <abanoub.wahib@verve.com>
2022-04-29 11:49:07 +0200

    Change whole project name to pubnative-max-ads-tester-app

---
+++ app/src/main/java/net/pubnative/lite/pubnative_max_ads_tester_app_android/MyApp.kt
@@ -0,0 +1,7 @@
+package net.pubnative.lite.pubnative_max_adss_tester_app_android
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApp: Application()
