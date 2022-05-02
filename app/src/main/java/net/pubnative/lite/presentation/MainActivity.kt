5742f3ddab5cf2a6f25429bd41972002838d9258
Abanoub Barayo <abanoub.wahib@verve.com>
2022-05-02 16:32:41 +0200

    Add Applovin sdk dependency and add meta data inside AndroidManifest.xml

--- app/src/main/java/net/pubnative/lite/presentation/MainActivity.kt
+++ app/src/main/java/net/pubnative/lite/presentation/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showSystemUi = true, showBackground = true)
 @Composable
 fun DefaultPreview() {
     MaxAdsMainTheme {
--- app/src/main/java/net/pubnative/lite/presentation/composables/BannerScreen.kt
+++ app/src/main/java/net/pubnative/lite/presentation/composables/BannerScreen.kt
@@ -10,9 +10,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import net.pubnative.lite.R
 
+@Preview(showSystemUi = true, showBackground = true)
 @Composable
 fun BannerScreen() {
     Column(
