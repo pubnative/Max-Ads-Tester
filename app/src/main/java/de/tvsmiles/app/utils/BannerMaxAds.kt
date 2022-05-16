15d1979c352e7ca30db3837b6445f1a2f17a117a
Abanoub Barayo <abanoub.wahib@verve.com>
2022-05-16 15:07:47 +0200

    remove loadAd inside onAdDisplayFailed

--- app/src/main/java/de/tvsmiles/app/utils/BannerMaxAds.kt
+++ app/src/main/java/de/tvsmiles/app/utils/BannerMaxAds.kt
@@ -15,46 +15,37 @@ fun loadBannerAd(maxAdView: MaxAdView?) {
 
     maxAdView?.setListener(object : MaxAdViewAdListener {
         override fun onAdLoaded(ad: MaxAd?) {
-            Log.d(TAG, "onAdLoaded: Success")
-            Toast.makeText(
-                maxAdView.context,
-                "Banner Ad is loaded successfully",
-                Toast.LENGTH_SHORT
-            ).show()
-        }
-
-        override fun onAdDisplayed(ad: MaxAd?) {
-            Log.d(TAG, "onAdDisplayed: ")
-        }
-
-        override fun onAdHidden(ad: MaxAd?) {
-            Log.d(TAG, "onAdHidden: ")
+            Log.d(TAG, "onAdLoaded")
         }
 
         override fun onAdClicked(ad: MaxAd?) {
-            Log.d(TAG, "onAdClicked: ")
+            Log.d(TAG, "onAdClicked")
         }
 
         override fun onAdLoadFailed(adUnitId: String?, error: MaxError?) {
-            Log.d(TAG, "onAdLoadFailed: Failed")
-            Toast.makeText(
-                maxAdView.context,
-                "Failed to load banner ad ,please try again later",
-                Toast.LENGTH_SHORT
-            ).show()
+            Log.d(TAG, "onAdLoadFailed")
             maxAdView.loadAd()
         }
 
         override fun onAdDisplayFailed(ad: MaxAd?, error: MaxError?) {
-            Log.d(TAG, "onAdDisplayFailed: ")
+            Log.d(TAG, "onAdDisplayFailed")
+            maxAdView.loadAd()
         }
 
         override fun onAdExpanded(ad: MaxAd?) {
-            Log.d(TAG, "onAdExpanded: ")
+            Log.d(TAG, "onAdExpanded")
         }
 
         override fun onAdCollapsed(ad: MaxAd?) {
-            Log.d(TAG, "onAdCollapsed: ")
+            Log.d(TAG, "onAdCollapsed")
+        }
+
+        override fun onAdDisplayed(ad: MaxAd?) {
+            Log.d(TAG, "onAdDisplayed")
+        }
+
+        override fun onAdHidden(ad: MaxAd?) {
+            Log.d(TAG, "onAdHidden")
         }
 
     })
